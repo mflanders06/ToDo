9fd0e1192eb6a759bac6ed209fb9a42e36d8d8cc
MFlanders <mflanders06@gmail.com>
2020-10-26 20:36:19 -0600

    Initial setup (input working

---
+++ index.js
@@ -0,0 +1,6 @@
+function submit(taskName, taskDescription, taskDate) {
+    // As you can see i'm putting all the console.logs in the same object here
+    // and since both the object and the properties name are the same i only have
+    // to specify them ones. 
+    console.log({ taskName, taskDescription, taskDate })
+}
