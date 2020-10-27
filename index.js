5725968cbf915891383ceb07157bdc60da3902bd
MFlanders <mflanders06@gmail.com>
2020-10-26 20:53:22 -0600

    Input passing into array

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+let taskArray = [];
+let inputObject = new Object();
+
 function submit(taskName, taskDescription, taskDate) {
     // As you can see i'm putting all the console.logs in the same object here
     // and since both the object and the properties name are the same i only have
-    // to specify them ones. 
-    console.log({ taskName, taskDescription, taskDate })
+    // to specify them once. 
+    
+    //console.log({ taskName, taskDescription, taskDate })
+
+    inputObject.ItemName = taskName;
+    inputObject.ItemDescription = taskDescription;
+    inputObject.ItemDate = taskDate;
+
+    //console.log(inputObject);
+    taskArray.push(inputObject);
+
+    console.log(taskArray);
 }
