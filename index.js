7d68c17ee111e0d60132354c839e5d8b6c2a79d3
MFlanders <mflanders06@gmail.com>
2020-11-02 20:26:57 -0700

    Taking input... need to expand scope of inputObj

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-//const taskArray = [];
-
 
 function submit(taskName, taskDescription, taskDate) {
-    // As you can see i'm putting all the console.logs in the same object here
-    // and since both the object and the properties name are the same i only have
-    // to specify them once. 
-    
-    //console.log({ taskName, taskDescription, taskDate })
 
-   let inputObject = new takeInput(taskName, taskDescription, taskDate);
+    if (typeof inputObject === 'undefined'){
+
+        let inputObject = new takeInput(taskName, taskDescription, taskDate);   
+        console.log(inputObject);        
+    }
+
+    else{
+
+        inputObject.push(new takeInput(taskName, taskDescription, taskDate));
+        console.log(inputObject);
+
+    }
 
 
 
-    console.log(inputObject);
-    //taskArray.push(inputObject);
-
-
-    //console.log(taskArray);
 }
 
 
